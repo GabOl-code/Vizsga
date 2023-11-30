9b83eaf5729df3698d29bb7de4c62b6802e34dd2
GabOl-code <73078610+GabOl-code@users.noreply.github.com>
2023-11-30 13:32:46 +0100

    Vizsga

--- titkos.py
+++
@@ -1,9 +0,0 @@
-
-class Titkos:
-    pass
-    def __init__(self):
-        self._titkos_attributum = "Titkos üzenet"
-    def kiir(self):
-        print(self._titkos_attributum)
-obj = Titkos()
-obj.kiir()
